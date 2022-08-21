726a001aa61026fd03e6f6022d38d7c4e55eae77
newDan1 <danielimanihalter@gmail.com>
2022-08-21 14:27:39 -0400

    Added help message. Added more error handling.

--- main.py
+++ main.py
@@ -2,16 +2,19 @@
 
 # TODO
 # Add comments to explain code
+# Add commands
+#   del [number]
+#   add [number] [value]
+#   copy [number]
+#   paste [number]
+#   edit [number]
 
 # BUG(s)
 # Some colored text is broken:
 #   quit message
 #   edit sub-item
 
-from msilib.schema import File
-from queue import Empty
-
-from errors import EmptyStringError
+from errors import EmptyStringError, InvalidFilenameError
 
 import sys
 
@@ -31,11 +34,18 @@ def full_stack():
 try:
     # Python program to (hopefully) create a Enless Sky (https://endless-sky.github.io/) Save Editor
 
+    VERSION = "0.1"
+    VERSION_CHANGELOG = '''
+        Added help message.
+        Added more error handling.
+    '''
+
     from termcolor import colored
     import colorama as color
     color.init()
 
     import os
+    from pathlib import Path
 
     def splitByIndentation(data: str):
         indented = False
@@ -57,6 +67,28 @@ try:
             prevLine = line
         output.append(currentBlock)
         return output
+
+    def printHelpMessage():
+        
+        helpMessage = f'''
+Endless Sky Save Editor v{VERSION}:
+    Version changes:
+        {VERSION_CHANGELOG}
+    Help
+        On entry, a prompt will apear to enter the file path to the save you wish to edit.
+        Enter the path to the save file you wish to edit.
+            (save files are located in C:\\Users\\[YOUR USERNAME]\\AppData\\Roaming\\endless-sky\\saves)
+        The program will print out a list of all the properties of the save file in the format of '{color.Fore.GREEN}[0]{color.Fore.WHITE} collapsed outfitter'
+        Then the program will ask for the element you want to edit.
+            This will be the number in green.
+        After you enter the number, the program will print a more detailed version of the element in the format of '{color.Fore.GREEN}[1]{color.Fore.WHITE}     Ammunition'
+        Then the program will ask for the sub-element you want to edit.
+            This will be the number in green.
+        The program will ask for the new value, while providing the old one, eg: '{color.Fore.GREEN}[1]{color.Fore.WHITE} Previously: Ammunition. Enter new value (leave blank to cancel): '
+        You can repeat changing values untill you wish to save.
+            To save, 
+'''
+        print(helpMessage)
 
     index = 0
 
@@ -86,13 +118,36 @@ try:
     trying = True
     while trying:
         filename = input("File path to save file (check in %appdata%/roaming/endless-sky/saves): ")
+        if not filename:
+            continue
+        elif filename.lower().strip() == 'exit':
+                    quit()
+        elif filename.lower().strip() == '?' or filename.lower().strip() == 'help':
+            printHelpMessage()
+            continue
+
         try:
+            try:
+                Path(filename).resolve()
+            except (OSError, RuntimeError):
+                raise InvalidFilenameError()
             with open(filename, 'r') as f:
                 items = splitByIndentation(f.read())
                 printAvailableItems()
                 trying = False
         except FileNotFoundError:
             print(color.Fore.YELLOW + f"The file {filename} was not found.\n Check you spelling and see if the file exists." + color.Fore.WHITE)
+        except InvalidFilenameError:
+            print('Invalid filename.')
+        except OSError as e:
+            try:
+                if "Invalid argument:" in str(e):
+                    raise InvalidFilenameError()
+                else:
+                    raise e
+            except InvalidFilenameError:
+                print('Invalid filename.')
+            
 
     while True:
         trying = True
@@ -118,7 +173,7 @@ try:
                             for item_ in item:
                                 savedFile += item_
                                 savedFile += '\n'
-                                
+
                         elif type(item) == str:
                             savedFile += item
 
@@ -150,22 +205,35 @@ try:
 
                     quit()
 
+                elif itemIndex.lower().strip() == 'exit':
+                    quit()
+
+                elif itemIndex.lower().strip() == '?' or itemIndex.lower().strip() == 'help':
+                    printHelpMessage()
+                    continue
+
                 # itemIndex = int(input("Enter a valid indeger of an item to edit: "))
                 print('Invalid item.')
 
 except KeyboardInterrupt:
-    from termcolor import colored
-    import colorama as color
-    color.reinit()
-    print()
-    end = input(color.Fore.RED + "Are you sure you want to quit (y/n)? " + color.Fore.WHITE)
-    end = end.strip().lower()
-    if end == 'y' or end == 'yes':
+    try:
+        from termcolor import colored
+        import colorama as color
+        color.reinit()
+        print()
+        end = input(f"{color.Fore.RED}Are you sure you want to quit (y/n)? {color.Fore.WHITE}")
+        end = end.strip().lower()
+        if end == 'y' or end == 'yes':
+            quit()
+        # else:
+        #     pass
+    except KeyboardInterrupt as e:
         quit()
-    # else:
-    #     pass
+    except BaseException as e:
+        print(color.Fore.RED + f"\n\nFATAL ERROR DETECTED!\n Please report this error to https://github.com/newDan1/endless-sky-save-editor/issues:\n{full_stack()}\n\n")
+        quit()
 except SystemExit as e:
     sys.exit(e)
 except BaseException as e:
-    print(color.Fore.RED + f"FATAL ERROR DETECTED!\n Please report this error to https://github.com/newDan1/endless-sky-save-editor/issues:\n{full_stack()}")
+    print(color.Fore.RED + f"\n\nFATAL ERROR DETECTED!\n Please report this error to https://github.com/newDan1/endless-sky-save-editor/issues:\n{full_stack()}\n\n")
     quit()
